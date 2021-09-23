4b1a3d7a4952ab74ce7146901484bc70c91dc35e
leo.hrb <leo.hrb@eduge.ch>
2021-09-23 16:07:53 +0200

    Version 2 bluetooth

--- sw.js
+++ sw.js
@@ -1,41 +1,42 @@
 var cacheName = 'hello-pwa';
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js'
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
 
     navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
-.then(device => { console.log("Succes"); })
-.catch(error => { console.log("Error") });
+        .then(device => { console.log("Succes"); })
+        .catch(error => { console.log("Error") });
 
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-  self.skipWaiting();
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+    self.skipWaiting();
 });
 
-self.addEventListener('load', function(e) {
-console.log("start");
-    navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
-.then(device => { document.getElementById("message").innerHTML = "Succes"; })
-.catch(error => { document.getElementById("message").innerHTML = "Error"; });
-console.log("done");
-});
+
 
 
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', function (e) {
+
+    console.log("start");
+    navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
+        .then(device => { document.getElementById("message").innerHTML = "Succes 2"; })
+        .catch(error => { document.getElementById("message").innerHTML = "Error 2"; });
+    console.log("done");
+
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
 });
